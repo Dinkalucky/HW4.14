3b3e7761548f9db9d1386c2f4e3e4e1bd359c8d7
Dina Nevhod <Dina_Nevhod@epam.com>
2023-05-02 13:42:50 +0200

    Add project files.

---
+++ 001_Thread_Join/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSample
+{
+    class Program
+    {
+        static void Function(string text)
+        {
+                Console.WriteLine("ID " + text + " потік: {0}", Thread.CurrentThread.ManagedThreadId);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                for (int i = 0; i < 50; i++)
+                {
+                    Thread.Sleep(20);
+                    Console.Write(".");
+                }
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine(text + " потік завершився.");
+            
+        }
+
+        static async Task Main() // Метод Main - виконується у первинному потоці.
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            
+            Task thread1 = new Task(()=>Function("Первинний"));
+            thread1.Start();
+            await thread1;
+            Task thread2 = new Task(() => Function("Вторинний"));
+            thread2.Start();
+            await thread2;
+            Task thread3 = new Task(() => Function("Третинний"));
+            thread3.Start();
+            await thread3;
+
+            // Очікування первинного потоку, завершення роботи вторинного потоку.
+            //thread.Join(); //TODO Зняти чи встановити коментар.
+
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task14.2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Task14._2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void timer_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = "Дані отримані";
+        }
+        
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.Sleep(4000);
+            textBox1.Text = "Підключено до бази даних";
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 0, 3);
+            timer.Start();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Thread.Sleep(4000);
+            textBox1.Text = "Підключення завершено";
+            timer.Stop();
+        }
+    }
+}
